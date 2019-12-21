d1ea6f381d4e93cdf03d7653f34d54ce8328a1e8
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-21 13:40:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,12 @@
 # code your #valid_move? method here
 def position_taken()
   if board[index] = " " || "" then TRUE
-  else nil
+  else FALSE
 end
 
 def some_new_fabulous_method
-  if position_taken
+  if position_taken = FALSE then
+    
     
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
